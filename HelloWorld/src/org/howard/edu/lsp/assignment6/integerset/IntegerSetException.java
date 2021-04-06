e5bf0d37cca52e5bd387e967dc18066612e8b75f
khalilSS <khalil.scottshepher@bison.howard.edu>
2021-04-06 00:59:34 -0400

    assignment 6

---
+++ HelloWorld/src/org/howard/edu/lsp/assignment6/integerset/IntegerSetException.java
@@ -0,0 +1,8 @@
+package org.howard.edu.lsp.assignment6.integerset;
+
+public class IntegerSetException extends Exception {
+	
+	public IntegerSetException(String message) {
+		super(message);
+	}
+}
